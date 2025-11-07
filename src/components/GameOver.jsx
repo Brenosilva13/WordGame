4f7368ce4d4c35429682e2595830dea271790738
Brenosilva13 <157593253+Brenosilva13@users.noreply.github.com>
2025-11-07 17:23:41 -0300

    feat: subindo projeto base

---
+++ src/components/GameOver.jsx
@@ -0,0 +1,15 @@
+import "./GameOver.css";
+
+const GameOver = ({ retry, score }) => {
+  const highScore = localStorage.getItem("highscore") || 0;
+
+  return (
+    <div className="gameover">
+      <h2>Fim de jogo!</h2>
+      <p>A sua pontuação foi: <span>{score}</span>!</p>
+      <p>Pontuação Máxima: <span>{highScore}</span> 👑</p>
+      <button onClick={retry}>Tentar Novamente</button>
+    </div>
+  );
+};
+export default GameOver;
